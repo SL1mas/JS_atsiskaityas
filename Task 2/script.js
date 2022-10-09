dfb6275985bf5118ec8e40b6c9b3685e6597fc3e
SL1mas <sslimass@gmail.com>
2022-10-09 19:42:30 +0300

    Task 2: added "Click me" counter in JS.

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,9 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+let clickNumber = 0;
+
+document.getElementById("btn__element").addEventListener("click", () => {
+  clickNumber++;
+  document.getElementById("btn__state").innerText = clickNumber;
+});
