602928266ba625f74927089851ee8432e1d08778
SL1mas <sslimass@gmail.com>
2022-10-10 23:30:29 +0300

    Task 1: added more style. Task 3: finished whole task.

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,31 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const showUsers = (users) => {
+  const output = document.getElementById("output");
+  output.innerText = "";
+  users.forEach((users) => {
+    const login = document.createElement("td");
+    login.innerText = users.login;
+    const avatar_url = document.createElement("td");
+    avatar_url.innerText = users.avatar_url;
+    const tr = document.createElement("tr");
+    tr.append(login, avatar_url);
+    output.append(tr);
+  });
+};
+
+const fetchUsers = async (event) => {
+  event.preventDefault();
+  try {
+    const response = await fetch("https://api.github.com/users");
+    if (response.ok) {
+      const users = await response.json();
+      showUsers(users);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+// fetchUsers();
+document.getElementById("btn").addEventListener("click", fetchUsers);
