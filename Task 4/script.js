f82db6cdd643500c56ddfa090fa46dec79a7ddb5
SL1mas <sslimass@gmail.com>
2022-10-10 23:57:45 +0300

    Task 4: created car brand and model list.

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,5 +8,31 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
+const showAuto = (auto) => {
+  const output = document.getElementById("output");
+  auto.forEach((auto) => {
+    const brand = document.createElement("td");
+    brand.innerText = auto.brand;
+    const models = document.createElement("td");
+    models.innerText = auto.models;
+    const tr = document.createElement("tr");
+    tr.append(brand, models);
+    output.append(tr);
+  });
+};
 
-const ENDPOINT = 'cars.json';
+const fetchAuto = async () => {
+  try {
+    const response = await fetch("cars.json");
+    if (response.ok) {
+      const users = await response.json();
+      showAuto(users);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchAuto();
+
+// const ENDPOINT = 'cars.json';
